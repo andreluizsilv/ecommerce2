f7ee2dcb0a79ac30485c1d4e839e093635c4a8d4
andreluizsilv <andre_luiz29@hotmail.com>
2024-06-09 19:25:28 -0300

    erro static

--- loja/urls.py
+++ loja/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
 from django.contrib.auth import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', homepage, name="homepage"),
@@ -34,5 +37,7 @@ urlpatterns = [
     path("reset/<uidb64>/<token>/", views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path("reset/done/", views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
